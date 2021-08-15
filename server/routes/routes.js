fc1a6462fd9f9f8e5e052e6545c8644f46f3e9ea
Tomi Salomaa <tomi.e.salomaa@utu.fi>
2021-08-15 16:59:09 +0300

    cleaned code

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const router = express.Router()
-const API = require("../controllers/api.js")
+const express = require("express");
+const router = express.Router();
+const API = require("../controllers/api.js");
 
-router.get("/vaccinations", API.getAllVaccinations)
-router.get("/daily-vaccinations", API.getVaccinationsPerDay)
-router.get("/vaccinations-gender", API.getVaccinationsByGender)
-router.get("/antiqua", API.getAllAntiqua)
-router.get("/antiqua-districts", API.getAntiquaDistricts)
-router.get("/solar", API.getAllSolar)
-router.get("/solar-districts", API.getSolarDistricts)
-router.get("/zerpfy", API.getAllZerpfy)
-router.get("/zerpfy-districts", API.getZerpfyDistricts)
+router.get("/vaccinations", API.getAllVaccinations);
+router.get("/daily-vaccinations", API.getVaccinationsPerDay);
+router.get("/vaccinations-gender", API.getVaccinationsByGender);
+router.get("/antiqua", API.getAllAntiqua);
+router.get("/antiqua-districts", API.getAntiquaDistricts);
+router.get("/solar", API.getAllSolar);
+router.get("/solar-districts", API.getSolarDistricts);
+router.get("/zerpfy", API.getAllZerpfy);
+router.get("/zerpfy-districts", API.getZerpfyDistricts);
 
-module.exports = router
+module.exports = router;
